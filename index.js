1398f11d4da21dcf4d021fb2f3037ee552be27ca
Joe Dietrich <joe@joedietrich.net>
2021-05-30 16:08:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//   console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, eventName) {
+  const cards = [];
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return cards;
+}
+
+console.log(writeCards(['Ali', 'Beth', 'Caleb', 'Dieter', 'Eunice', 'Freddy'], 'festivus'));
+
+const countDown = (num) => {
+  while (num >= 0){
+  console.log(num--);
+}
+}
+
+countDown(15)
